0eb531d89704e380d8e975c5f301f96134e1c7e3
VLtim43 <fernandoreal.valentim@gmail.com>
2023-08-13 22:30:57 -0300

    Add zutand

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import * as S from "./styles";
 import { colors } from "../utils/colors";
-import MultiStepForm from "./MultiStep";
+import useFormStore from "../../Zustand/store";
 
 const Form = () => {
+  const { currentStep, goToStep, resetForm } = useFormStore();
+
+  const handleNextStep = () => {
+    const nextStep = currentStep + 1;
+    if (nextStep <= 2) {
+      goToStep(nextStep);
+    }
+  };
+
+  const handlePreviousStep = () => {
+    const previousStep = currentStep - 1;
+    if (previousStep >= 0) {
+      goToStep(previousStep);
+    }
+  };
+
   return (
     <S.FormContainer backgroundColor={colors.palePink}>
-      <S.MainContainer></S.MainContainer>
+      <S.MainContainer>
+        {currentStep === 0 && <p>one</p>}
+        {currentStep === 1 && <p>two</p>}
+        {currentStep === 2 && <p>three</p>}
+      </S.MainContainer>
 
-      <S.ButtonContainer
-        backgroundColor={colors.lightCream}
-      ></S.ButtonContainer>
+      <S.ButtonContainer backgroundColor={colors.lightCream}>
+        {currentStep > 0 && (
+          <button onClick={handlePreviousStep}>Previous</button>
+        )}
+        {currentStep < 2 ? (
+          <button onClick={handleNextStep}>Next</button>
+        ) : (
+          <button onClick={resetForm}>Reset</button>
+        )}
+      </S.ButtonContainer>
     </S.FormContainer>
   );
 };
